55ec09191b95d403062f4c61673d54f3fa911f99
Kelly Aguilar <kelly.marie.aguilar@gmail.com>
2020-07-09 07:25:36 -0600

    [ADD] course.py

---
+++ openacademy/models/course.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+
+#     name = fields.Char()
+
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
