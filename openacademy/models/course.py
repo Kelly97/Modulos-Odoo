5ce107d96490d3dc6ffb138620f46817da5011ad
Kelly Aguilar <kelly.marie.aguilar@gmail.com>
2020-07-19 15:21:41 +0000

    [ADD] Default values

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -24,9 +24,10 @@ class Session(models.Model):
     _description = "OpenAcademy Sessions"
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
 
     instructor_id = fields.Many2one('res.partner', string="Instructor",
         domain=['|', ('instructor', '=', True),
@@ -34,7 +35,7 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
-    
+
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
 
     @api.depends('seats', 'attendee_ids')
